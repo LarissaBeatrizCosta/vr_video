daf617eb87a66899c05d102632253cb1509980a1
larissa beatriz costa <larissa@grupovex.com.br>
2025-10-16 08:05:30 -0300

    feat: :sparkles: init project

---
+++ lib/conts/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+///Class responsable for theme of all app
+class AppTheme{
+  AppTheme._();
+
+  static ThemeData themeData(){
+    return ThemeData(
+      
+    );
+  }
+}
